be1de00cdc9f05c14ecd6b6a8bfb790dcb273ece
Ivanka <ivanka.polyuhovych@gmail.com>
2023-02-28 13:29:05 +0200

    Add cinemachine

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core.Tools;
+using Core.Enum;
 
 namespace Player
 {
@@ -9,7 +11,7 @@ namespace Player
     {
         [Header("HorizontalMovement")]
         [SerializeField] private float _horizontalSpeed;
-        [SerializeField] private bool _faceRight;
+        [SerializeField] private Direction _direction;
 
         [Header("VerticalMovement")]
         [SerializeField] private float _verticalSpeed;
@@ -19,6 +21,9 @@ namespace Player
         [Header("Jump")]
         [SerializeField] private float _jumpForce;
         [SerializeField] private float _gravityScale;
+
+
+        [SerializeField] private DirectionalCameraPair _cameras;
 
         private Rigidbody2D _rigidbody;
 
@@ -73,7 +78,8 @@ namespace Player
 
         private void SetDirection(float direction)
         {
-            if((_faceRight && direction < 0) || (!_faceRight && direction > 0))
+            if((_direction == Direction.Right && direction < 0) || 
+                (_direction == Direction.Left && direction > 0))
             {
                 Flip();
             }
@@ -82,7 +88,9 @@ namespace Player
         private void Flip()
         {
             transform.Rotate(0, 180, 0);
-            _faceRight = !_faceRight;
+            _direction = _direction == Direction.Right ? Direction.Left : Direction.Right;
+            foreach (var cameraPair in _cameras.DirectionalCameras)
+                cameraPair.Value.enabled = cameraPair.Key == _direction;
         }
 
         private void UpdateJump()
